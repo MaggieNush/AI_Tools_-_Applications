a6e53931a7ed0de460739fa539cd8ea065cb93df
wizard-hash <aronidengeno@gmail.com>
2025-06-15 23:04:52 +0300

    The task 1

---
+++ scikitlearn.py
@@ -0,0 +1,35 @@
+# Import  libraries
+from sklearn.datasets import load_iris
+from sklearn.model_selection import train_test_split
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.metrics import accuracy_score, precision_score, recall_score
+
+# Loading the Iris dataset
+iris = load_iris()
+X, y = iris.data, iris.target
+
+# Split the dataset into training and testing sets
+# test_size=0.3 means 30% of the data will be used for testing
+# random_state=42 ensures reproducibility of the split
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_state=42)
+
+# using  a Decision Tree Classifier model
+
+model = DecisionTreeClassifier(random_state=42)
+
+# Train the model using the training data
+model.fit(X_train, y_train)
+
+# predictions on the test set
+y_pred = model.predict(X_test)
+
+# Evaluate the model and Calculating accuracy
+accuracy = accuracy_score(y_test, y_pred)
+
+precision = precision_score(y_test, y_pred, average='macro')
+recall = recall_score(y_test, y_pred, average='macro')
+
+# Print the evaluation metrics
+print(f"Accuracy: {accuracy:.4f}")
+print(f"Precision: {precision:.4f}")
+print(f"Recall: {recall:.4f}")
